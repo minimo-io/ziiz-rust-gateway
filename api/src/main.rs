c871b477ffb3e898dd726e9ae89dfddfa17b8dae
minimo-io <nicolas@minimo.io>
2024-10-17 10:33:09 -0300

    comments on drop

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,21 +1,18 @@
-
-
-use actix_web::{get, web::Data, App, HttpServer, Responder, middleware::Logger, HttpResponse};
+use actix_web::{get, middleware::Logger, web::Data, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
 use sea_orm::{Database, DbConn};
 use std::env;
 use std::sync::Arc;
-use dotenv::dotenv;
 
 use ziiz::utils::date_utils::format_date;
 
-mod routes;
-pub mod entities; // export it for use among the app
+pub mod entities;
+mod routes; // export it for use among the app
 
 // api endpoints
-use routes::payin::pay;
 use routes::data::all_countries;
 use routes::home::home;
-
+use routes::payin::pay;
 
 #[derive(Clone)]
 struct AppState {
@@ -25,25 +22,30 @@ struct AppState {
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    // for debug, but still this should be retrieved from 
+    // for debug, but still this should be retrieved from
     // our settings toml files in the future
     std::env::set_var("RUST_LOG", "debug");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
-
     // this enviroment handling should go to the config mod
     // once that is done delete this
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in enviroment");
-    let app_server_addr = env::var("APP_SERVER__ADDR").expect("APP_SERVER__ADDR must be set in enviroment");
-    let app_server_port = env::var("APP_SERVER__PORT").expect("APP_SERVER__PORT must be set in enviroment");
+    let app_server_addr =
+        env::var("APP_SERVER__ADDR").expect("APP_SERVER__ADDR must be set in enviroment");
+    let app_server_port =
+        env::var("APP_SERVER__PORT").expect("APP_SERVER__PORT must be set in enviroment");
     let app_server_port = app_server_port.parse().unwrap_or(8080); // convert to a integer or 8080
 
-    println!("Runnin on address {}:{}", &app_server_addr, &app_server_port);
+    println!(
+        "Runnin on address {}:{}",
+        &app_server_addr, &app_server_port
+    );
 
-    let db: DbConn = Database::connect(&database_url).await.expect("Failed to connect to the database"); // for SeaORM
-    
-    
+    let db: DbConn = Database::connect(&database_url)
+        .await
+        .expect("Failed to connect to the database"); // for SeaORM
+
     // assert_eq!(db.ping().await.is_ok(), true, "Database ping failed!");
 
     if db.ping().await.is_ok() {
@@ -61,21 +63,21 @@ async fn main() -> std::io::Result<()> {
         let logger = Logger::default();
 
         App::new()
-        .wrap(logger)
-        .app_data(Data::new(db.clone())) // Store the database connection to access in services
-        // .app_data(Data::new(AppState {
-        //     db_pool: app_state.clone(),
-        // }))        
-        .service(pay)
-        .service(home)
-        .service(all_countries)
+            .wrap(logger)
+            .app_data(Data::new(db.clone())) // Store the database connection to access in services
+            // .app_data(Data::new(AppState {
+            //     db_pool: app_state.clone(),
+            // }))
+            .service(pay)
+            .service(home)
+            .service(all_countries)
     })
     .bind((app_server_addr.as_str(), app_server_port))?
     .run()
     .await
 }
 
-// CLOSE CONNECTION WHEN EXIT ???
+// CLOSE CONNECTION WHEN FORCE EXIT (SeaORM quits db on drop but...)
 // let db = Database::connect(url).await?;
 // Closing connection here
 // db.close().await?;
